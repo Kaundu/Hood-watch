1c926ce10bd1fa99a8d5e1fa715574ceb92ecdfe
neville <kilonzoneville@gmail.com>
2018-08-03 12:22:59 +0300

    add the models for the Neighborhood class

--- hood/models.py
+++ hood/models.py
@@ -10,3 +10,9 @@ class Profile(models.Model):
     last_name= models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     prof_pic = models.ImageField(upload_to= 'profiles/', blank=True)
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupants = models.IntegerField()
+    admin =  models.ForeignKey(User, related_name="made_by", on_delete=models.CASCADE)
