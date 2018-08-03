59ba8691f46abf190dc8bc3a5c5dc8740e1e7a0b
neville <kilonzoneville@gmail.com>
2018-08-03 12:13:03 +0300

    add the models for the Profile/User class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save,post_delete
+from django.dispatch import receiver
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50 )
+    last_name= models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    prof_pic = models.ImageField(upload_to= 'profiles/', blank=True)
