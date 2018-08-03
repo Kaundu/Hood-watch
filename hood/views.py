f1d2bf300fd5923d0453a8d183457d28052b20e2
neville <kilonzoneville@gmail.com>
2018-08-03 12:56:32 +0300

    add the initial views for the app hood

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from hood.models import  Profile,Neighborhood,Business
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    return HttpResponse("Waaauh ")
