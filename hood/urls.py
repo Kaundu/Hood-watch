1bce108fb81198ee2d84fc99cab4841c565fdd86
neville <kilonzoneville@gmail.com>
2018-08-10 11:22:41 +0300

    add the app url for the neighborhood view

--- hood/urls.py
+++ hood/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url('^profile/(?P<user_id>\d+)', views.profile, name='profile'),
+    url('^neighborhood/(?P<neighborhood_id>\d+)', views.neighborhood, name='neighborhood'),
 ]
 # if settings.DEBUG:
 #     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
