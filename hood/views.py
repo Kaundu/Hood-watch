19ce4ba5afb7ece1cb71b795e9c0f5b732fa5618
neville <kilonzoneville@gmail.com>
2018-08-10 11:18:51 +0300

    add the necessary inputs for the index page view function

--- hood/views.py
+++ hood/views.py
@@ -6,4 +6,9 @@ from hood.models import  Profile,Neighborhood,Business
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
+    return render(request, 'index.html', locals())    hoods = Neighborhood.objects.all()
+    # users = User.objects.get(id=user_id)
     return render(request, 'index.html', locals())
+
+# def neighborhood(request):
+
