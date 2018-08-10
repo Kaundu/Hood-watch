e8b17acd427d8e0f69e72098af803b88bd2404a3
neville <kilonzoneville@gmail.com>
2018-08-10 11:25:31 +0300

    add the app url for the new-neighborhood view

--- hood/urls.py
+++ hood/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url('^updateprofile$', views.updateprofile , name='updateprofile' ),
+    url('^newneighborhood$' , views.new_neighborhood, name='new_neighborhood'),
     url('^profile/(?P<user_id>\d+)', views.profile, name='profile'),
     url('^neighborhood/(?P<neighborhood_id>\d+)', views.neighborhood, name='neighborhood'),
     url('^business/(?P<business_id>\d+)' , views.business, name='business'),
