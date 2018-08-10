f0ffb43c0193bb87465d5558ff3f61afa6048bc6
neville <kilonzoneville@gmail.com>
2018-08-10 11:19:41 +0300

    add the view function to allow one to view a profile

--- hood/views.py
+++ hood/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, HttpResponse,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .models import *
+from .forms import  ProfileForm, NeighborhoodForm,BusinessForm,PostForm
 from hood.models import  Profile,Neighborhood,Business
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html', locals())    hoods = Neighborhood.objects.all()
+    hoods = Neighborhood.objects.all()
     # users = User.objects.get(id=user_id)
     return render(request, 'index.html', locals())
 
 # def neighborhood(request):
 
+
+
+@login_required(login_url='/accounts/login')
+def profile(request, user_id):
+	users = User.objects.get(id = user_id)
+	profile = Profile.objects.get(user= users)
+	print(profile)
+	return render(request, 'profile.html', locals())
