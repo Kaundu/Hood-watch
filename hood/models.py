e26226c9ba941e91f812ae30535235e2e7a986a9
neville <kilonzoneville@gmail.com>
2018-08-10 11:15:39 +0300

    add the model class for a post

--- hood/models.py
+++ hood/models.py
@@ -25,3 +25,12 @@ class Business(models.Model):
     email = models.CharField(max_length=50)
     owner =  models.ForeignKey(User, related_name="ownde_by", on_delete=models.CASCADE)
     hood = models.ForeignKey(Neighborhood,  on_delete=models.CASCADE)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField(max_length=200, blank=True, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True, related_name='posted_by')
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='posts_for')
+
+    def __str__(self):
+        return self.title
